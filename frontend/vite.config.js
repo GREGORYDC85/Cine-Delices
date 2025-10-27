f87fdc70316077137957428f0cac0dc26de7ee24
GREGORYDC85 <gdrivoncollin@gmail.com>
2025-10-27 21:32:08 +0100

    Fix vite base path for GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// 📌 Définit un port stable pour Vite
+// ✅ Configuration pour GitHub Pages
 export default defineConfig({
+  base: "/Cine-Delices/", // <-- important pour GitHub Pages
   plugins: [react()],
   server: {
     port: parseInt(process.env.VITE_FRONTEND_PORT) || 5173,
